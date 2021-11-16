5ad7799739ef8a7ab17f711ebcc43a6b1eee2954
anjali <ag450081@gmail.com>
2021-11-16 16:20:28 +0530

    Separete Redux for all Components

--- src/User/Todo.js
+++ src/User/Todo.js
@@ -1,46 +1,46 @@
 import React from "react";
 import axios from "axios";
-import { connect } from "react-redux";
-import { todos } from "../actions/index";
+import { connect } from 'react-redux';
+import {todos} from '../actions/index';
 import { ListGroup } from "react-bootstrap";
 class TodoUser extends React.Component {
   todoData = async (id) => {
     const todo = await axios.get(`http://localhost:3008/users/${id}/todos`);
-    this.props.todos(todo.data);
+    this.props.todos(todo.data)
   };
   componentDidMount() {
     const { id } = this.props.match.params;
     this.todoData(id);
   }
   render() {
-    const todoData = this.props.todos.todo;
+    const todoData = this.props.mytodo.todo;
+    const todos = todoData.map((data) => {
+      return (
+        <ul key={data.id}>
+          <li style={{ listStyleType: "square" }}>
+            <ListGroup>
+              <ListGroup.Item>{data.title}</ListGroup.Item>
+            </ListGroup>
+          </li>
+        </ul>
+      );
+    });
     return (
       <>
         <div className="container my-3">
           <h3 className="text-center my-2 text-danger">Todos...</h3>
-          {todoData.map((data) => (
-            <ul key={data.id}>
-              <li style={{ listStyleType: "square" }}>
-                <ListGroup>
-                  <ListGroup.Item>{data.title}</ListGroup.Item>
-                </ListGroup>
-              </li>
-            </ul>
-          ))}
+          {todos}
         </div>
       </>
     );
   }
 }
-const mapDispatchToProps = {
-  todos,
-};
-const mapStateToProps = (state) => ({
-  todos: state.todo,
-});
-const todosConnectedWithRedux = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TodoUser);
+const mapDispatchToProps={
+  todos
+ }
+ const mapStateToProps=(state)=>({
+  mytodo:state.todo
+})
+const todosConnectedWithRedux = connect(mapStateToProps, mapDispatchToProps)(TodoUser);
 
-export default todosConnectedWithRedux;
+export default todosConnectedWithRedux
